bfae7be2392089f0e0ed336db68e99f1ab3b4852
Johannes Zarl <johannes.zarl@jku.at>
2014-06-16 18:25:52 +0200

    Update version number.

--- src/unsharedfs.c
+++ src/unsharedfs.c
@@ -16,7 +16,7 @@
  * A copy of that code is included in the file fuse.h
  */
 
-#define UNSHAREDFS_VERSION_STRING "unsharedfs 1.1"
+#define UNSHAREDFS_VERSION_STRING "unsharedfs 1.2git"
 
 #include "fs.h"
 
